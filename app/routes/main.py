9137a17ef71cada001a19f52d65e84d93dcf11d8
Emerson Taymor <emerson.taymor@gmail.com>
2025-02-28 20:37:19 -0500

    feat: add dynamic sitemap.xml generation and about page - Added dynamic sitemap.xml generation for better SEO - Created about page with personal info and LinkedIn link - Added sitemap reference in base template

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,11 +1,55 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, make_response, url_for
 from config.tools import get_other_tools, get_tools_by_category
+from datetime import datetime
+import urllib.parse
 
 main_bp = Blueprint('main', __name__)
+
+def get_all_routes():
+    """Get all routes for the sitemap"""
+    routes = []
+    
+    # Add static routes
+    static_routes = ['index', 'about', 'value_proposition', 'user_story']
+    for route in static_routes:
+        url = url_for(f'main_bp.{route}', _external=True)
+        routes.append({
+            'loc': url,
+            'lastmod': datetime.now().strftime('%Y-%m-%d'),
+            'priority': '0.8' if route == 'index' else '0.6'
+        })
+    
+    # Add dynamic tool routes from config
+    tools_by_category = get_tools_by_category()
+    for category in tools_by_category:
+        for tool in tools_by_category[category]:
+            url = url_for('main_bp.index', _external=True) + urllib.parse.quote(tool['url'].lstrip('/'))
+            routes.append({
+                'loc': url,
+                'lastmod': datetime.now().strftime('%Y-%m-%d'),
+                'priority': '0.7'
+            })
+    
+    return routes
+
+@main_bp.route('/sitemap.xml')
+def sitemap():
+    """Generate sitemap.xml"""
+    routes = get_all_routes()
+    
+    xml_content = render_template('sitemap.xml', routes=routes)
+    response = make_response(xml_content)
+    response.headers["Content-Type"] = "application/xml"
+    
+    return response
 
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/about')
+def about():
+    return render_template('about.html')
 
 @main_bp.route('/value-proposition')
 def value_proposition():
